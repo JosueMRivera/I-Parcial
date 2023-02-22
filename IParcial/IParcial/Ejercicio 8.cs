13916c8df42939acea76e51d7aeefb4f73c7290e
Josue Rivera <jm.rivera@unah.hn>
2023-02-22 07:31:02 -0600

    Primer Commit
    Creación de la recursividad y su uso en lo s numeros del 1 al 100

---
+++ IParcial/IParcial/Ejercicio 8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio_8 : Form
+    {
+        public Ejercicio_8()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            recorrerdeunoenuno(1, 100);
+        }
+
+        private void recorrerdeunoenuno(int valinicial, int valfinal)
+        {
+            listBox1.Items.Add(valinicial);
+
+            if (valinicial < valfinal)
+            {
+                recorrerdeunoenuno(valinicial + 1, valfinal);
+            }
+            
+        }
+
+    }
+}
--- IParcial/IParcial/Program.cs
+++ IParcial/IParcial/Program.cs
@@ -16,7 +16,7 @@ namespace IParcial
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Ejercicio_7());
+            Application.Run(new Ejercicio_8());
         }
     }
 }
