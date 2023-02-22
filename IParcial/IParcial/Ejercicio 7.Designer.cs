91465d84bced1b33294a588029d6bc80d98c7efc
Josue Rivera <jm.rivera@unah.hn>
2023-02-22 07:13:07 -0600

    Primer Commit
    Creacion de una matriz y mostrando el tamaño ya sea introducidos por el usuario o una matriz con un tamaño random

---
+++ IParcial/IParcial/Ejercicio 7.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace IParcial
+{
+    partial class Ejercicio_7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tamañodematriztxt = new System.Windows.Forms.TextBox();
+            this.filatxt = new System.Windows.Forms.TextBox();
+            this.columnatxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(183, 368);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar Matriz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tamaño de Matriz:";
+            // 
+            // tamañodematriztxt
+            // 
+            this.tamañodematriztxt.Location = new System.Drawing.Point(352, 73);
+            this.tamañodematriztxt.Name = "tamañodematriztxt";
+            this.tamañodematriztxt.Size = new System.Drawing.Size(100, 20);
+            this.tamañodematriztxt.TabIndex = 3;
+            // 
+            // filatxt
+            // 
+            this.filatxt.Location = new System.Drawing.Point(572, 46);
+            this.filatxt.Name = "filatxt";
+            this.filatxt.Size = new System.Drawing.Size(100, 20);
+            this.filatxt.TabIndex = 4;
+            // 
+            // columnatxt
+            // 
+            this.columnatxt.Location = new System.Drawing.Point(572, 72);
+            this.columnatxt.Name = "columnatxt";
+            this.columnatxt.Size = new System.Drawing.Size(100, 20);
+            this.columnatxt.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(540, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fila:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(515, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Columna:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(577, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tamaño de Matriz";
+            // 
+            // Ejercicio_7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.columnatxt);
+            this.Controls.Add(this.filatxt);
+            this.Controls.Add(this.tamañodematriztxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Ejercicio_7";
+            this.Text = "Ejercicio_7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tamañodematriztxt;
+        private System.Windows.Forms.TextBox filatxt;
+        private System.Windows.Forms.TextBox columnatxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ IParcial/IParcial/Ejercicio 7.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio_7 : Form
+    {
+        public Ejercicio_7()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            //Matriz
+            int[,] matriz1 = new int[3, 3] { 
+                                              {3, 6, 3}, 
+                                              {4, 5, 6}, 
+                                              {7, 8, 9} 
+                                            };
+
+            int valorfila = Convert.ToInt32(filatxt.Text);
+            int valorcolumna = Convert.ToInt32(columnatxt.Text);
+
+            Random random = new Random();
+            //int valorfila = random.Next(1, 10);
+            //int valorcolumna = random.Next(1, 10);
+            int[,] matriz2 = new int[valorfila, valorcolumna];
+
+            for (int fila = 0; fila < matriz2.GetLength(0); fila++)
+            {
+                for (int columna = 0; columna < matriz2.GetLength(1); columna++)
+                {
+                    matriz2[fila, columna] = random.Next(1, 100);
+                }
+            }
+
+            for (int fila = 0; fila < matriz2.GetLength(0); fila++)
+            {
+                for (int columna = 0; columna < matriz2.GetLength(1); columna++)
+                {
+                    listBox1.Items.Add("La posicion: [" + fila + "," + columna + "] = " + matriz2[fila, columna]);
+                }
+            }
+
+            tamañodematriztxt.Text = matriz2.GetLength(0).ToString() + "x" + matriz2.GetLength(1).ToString();
+
+        }
+    }
+}
